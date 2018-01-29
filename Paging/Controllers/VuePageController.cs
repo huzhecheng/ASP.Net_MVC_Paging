95e2b3ebaa3a17d3186f08d2103e0697b99154af
MRSFRED68E0\fred <v57801129@gmail.com>
2018-01-29 14:04:07 +0800

    Add Vue Page

---
+++ Paging/Controllers/VuePageController.cs
@@ -0,0 +1,28 @@
+﻿using Paging.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Paging.Controllers
+{
+	public class VuePageController : Controller
+	{
+		private DataService service = new DataService();
+
+		// GET: VuePage
+		public ActionResult VuePageDemo()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public JsonResult GetCustomer(int Page)
+		{
+			var data = service.GetData(Page);
+
+			return Json(new { data = data.Item1, pageinfo = data.Item2 });
+		}
+	}
+}
---
+++ Paging/Services/DataService.cs
@@ -0,0 +1,37 @@
+﻿using Paging.Interface;
+using Paging.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Paging.Extention;
+using Paging.Tool;
+
+namespace Paging.Services
+{
+	public class DataService
+	{
+		private NorthwindNETEntities db = new NorthwindNETEntities();
+
+		public IPageList<Customer> GetCustomer(int Page)
+		{
+			var data = db.Customer.OrderBy(x => x.ID).SkipTakeList(Page, 10);
+
+			return data;
+		}
+
+		public Tuple<object, PageInfo> GetData(int Page)
+		{
+			var data = db.Customer.Select(x => new
+			{
+				x.ID,
+				x.Name,
+				x.City,
+				x.Country,
+				x.Phone
+			}).OrderBy(x => x.ID).SkipTakeList(Page, 10);
+
+			return new Tuple<object, PageInfo>(data, data.pageinfo);
+		}
+	}
+}
